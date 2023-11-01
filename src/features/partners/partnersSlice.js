70adfc1a0065533c9d7720e57b3c0347d853b149
Jonathan Martinez <MartinezJonathan.DEV@gmail.com>
2023-11-01 09:47:04 -0700

    Redux for All

--- src/features/partners/partnersSlice.js
+++ src/features/partners/partnersSlice.js
@@ -1,9 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
 import { PARTNERS } from "../../app/shared/PARTNERS";
 
-export const selectAllPartners = () => {
-  return PARTNERS;
+const initialState = {
+  parternsArray: PARTNERS,
 };
 
-export const selectFeaturedPartner = () => {
-  return PARTNERS.find((partner) => partner.featured);
+const partnersSlice = createSlice({
+  name: "partners",
+  initialState,
+});
+export const partnersReducer = partnersSlice.reducer;
+
+export const selectAllPartners = (state) => {
+  return state.partners.parternsArray;
+};
+
+export const selectFeaturedPartner = (state) => {
+  return state.partners.parternsArray.find((partner) => partner.featured);
 };
