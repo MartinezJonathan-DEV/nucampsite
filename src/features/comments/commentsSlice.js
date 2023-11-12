bd928721b0e818be378b86e2969d061e6ab34b62
Jonathan Martinez <MartinezJonathan.DEV@gmail.com>
2023-11-12 14:07:42 -0800

    createAsyncThunk() comments

--- src/features/comments/commentsSlice.js
+++ src/features/comments/commentsSlice.js
@@ -1,8 +1,40 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { COMMENTS } from "../../app/shared/COMMENTS";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { baseUrl } from "../../app/shared/baseUrl";
+// import { COMMENTS } from "../../app/shared/COMMENTS";
+
+export const fetchComments = createAsyncThunk(
+  "comments/fetchComments",
+  async () => {
+    const response = await fetch(baseUrl + "comments");
+    if (!response.ok) {
+      return Promise.reject("Unable to fetch status:" + response.status);
+    }
+    const data = await response.json();
+    return data;
+  }
+);
+
+export const postComment = createAsyncThunk(
+  "comments/postComments",
+  async (comment, { dispatch }) => {
+    const response = await fetch(baseUrl + "comments", {
+      method: "POST",
+      body: JSON.stringify(comment),
+      headers: { "Content-Type": "application/json" },
+    });
+    console.log("response: ", response.ok);
+    if (!response.ok) {
+      return Promise.reject(response.status);
+    }
+    const data = await response.json();
+    dispatch(addComment(data));
+  }
+);
 
 const initialState = {
-  commentsArray: COMMENTS,
+  commentsArray: [],
+  isLoading: true,
+  errMsg: "",
 };
 
 const commentsSlice = createSlice({
@@ -19,6 +51,27 @@ const commentsSlice = createSlice({
       state.commentsArray.push(newComment);
     },
   },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchComments.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchComments.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.errMsg = "";
+        state.commentsArray = action.payload;
+      })
+      .addCase(fetchComments.rejected, (state, action) => {
+        state.isLoading = false;
+        state.errMsg = action.error ? action.error.message : "Failed to fetch";
+      })
+      .addCase(postComment.rejected, (state, action) => {
+        alert(
+          "Your comment could not be posted\nError: " +
+            (action.error ? action.error.message : "Fetch failed")
+        );
+      });
+  },
 });
 
 export const commentsReducer = commentsSlice.reducer;
@@ -27,6 +80,6 @@ export const { addComment } = commentsSlice.actions;
 
 export const selectCommentsByCampsiteId = (campsiteId) => (state) => {
   return state.comments.commentsArray.filter(
-    (comment) => comment.campsiteId === parseInt(campsiteId),
+    (comment) => comment.campsiteId === parseInt(campsiteId)
   );
 };
